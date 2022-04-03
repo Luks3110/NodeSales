69b49125a5d00ac77885d12882533f34c3594e03
luks3110 <lucas_haya@hotmail.com>
2022-04-02 21:25:39 -0300

    mail template with handlebars implementation

---
+++ src/config/mail/EtherealMail.ts
@@ -0,0 +1,61 @@
+import nodemailer from 'nodemailer';
+import HandlebarsMailTemplate from './HandlebarsMailTemplate';
+
+interface ITemplateVariable {
+  [key: string]: string | number;
+}
+
+interface IParseMailTemplate {
+  template: string;
+  variables: ITemplateVariable;
+}
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+interface ISendMail {
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplate;
+}
+
+export default class EtherealMail {
+  static async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMail): Promise<void> {
+    const account = await nodemailer.createTestAccount();
+
+    const mailTemplate = new HandlebarsMailTemplate();
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: 'velda.hettinger6@ethereal.email',
+        pass: '2YtNYz3FrcBNPsmtUf',
+      },
+    });
+
+    const message = await transporter.sendMail({
+      from: {
+        name: from?.name || 'Equipe ApiVendas',
+        address: from?.email || 'equipe@apivendas.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await mailTemplate.parse(templateData),
+    });
+
+    console.log(`Message sent: ${message.messageId}`);
+    console.log(`Preview URL: ${nodemailer.getTestMessageUrl(message)}}`);
+  }
+}
